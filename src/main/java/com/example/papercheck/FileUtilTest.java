ab8399bc38e7000d6ee7e6874e260f847ef84ea7
钟启腾 <3044227626@qq.com>
2024-09-14 01:28:33 +0800

    测试文件工具类功能

---
+++ src/main/java/com/example/papercheck/FileUtilTest.java
@@ -0,0 +1,30 @@
+package com.example.papercheck;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class FileUtilTest {
+    @Test
+    public void test01(){
+        try {
+            FileUtil.readFile("/Users/yiyan/Desktop/3122004761/src/main/resources/测试文本/aaa.txt");
+        } catch (IOException e) {
+            System.out.println("文件不存在");
+        }catch (FileIsNotTxtException e){
+            System.out.println("文件格式不对");
+        }
+    }
+
+    @Test
+    public void test02(){
+        try {
+            FileUtil.readFile("/Users/yiyan/Desktop/3122004761/src/main/resources/测试文本/orig.doc");
+        } catch (IOException e) {
+            System.out.println("文件不存在");
+        }catch (FileIsNotTxtException e){
+            System.out.println("文件格式错误");
+        }
+    }
+
+}
